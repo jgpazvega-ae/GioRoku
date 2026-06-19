1b92c67de58de5d6cb2624bfb0305b621b4fd8f3
jgpazvega-ae <jg.pazvega@gmail.com>
2026-06-19 11:39:19 -0600

    feat(backend): add services package

---
+++ backend/services/__init__.py
@@ -0,0 +1,6 @@
+from .aggregator import Aggregator
+from .validator import StreamValidator
+from .deduplicator import Deduplicator
+from .country_detector import CountryDetector
+from .logo_resolver import LogoResolver
+from .epg_manager import EPGManager
